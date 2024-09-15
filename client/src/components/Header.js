736317414ce798bdeb375effe83d140c4d510f12
24Hoshiko <2005gaganjotkaur@gmail.com>
2024-09-15 12:58:56 +0530

    bootstraping added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -16,22 +16,47 @@ function Header() {
     };
   }, []);
 
+  // Function to handle navbar link click, ensuring collapse closes on mobile view
+  const handleNavLinkClick = () => {
+    const navbarCollapse = document.getElementById('navbarSupportedContent');
+    const bsCollapse = new window.bootstrap.Collapse(navbarCollapse, {
+      toggle: false
+    });
+    bsCollapse.hide(); // Close the dropdown
+  };
+
   return (
     <header className={`fixed-top ${scrolled ? 'scrolled' : ''}`}>
       <nav className={`navbar navbar-expand-lg navbar-dark px-0 mx-0 ${scrolled ? 'navbar-dark-scrolled' : ''}`}>
-        <div className="container py-0 px-0 align-content-center  mx-5">
-          <Link className="navbar-brand " to="/">
+        <div className="container py-0 px-0 align-content-center mx-5">
+          <Link className="navbar-brand" to="/">
             <img src="/logo.png" alt="Logo" className="logo-img" />
           </Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon "></span>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0 py-3 justify-content-end">
-              <li className="nav-item"><Link className="nav-link" to="/">Home</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/events">Events</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/about">About Us</Link></li>
-              <li className="nav-item"><Link className="nav-link" to="/contact">Contact</Link></li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/" onClick={handleNavLinkClick}>Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/events" onClick={handleNavLinkClick}>Events</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about" onClick={handleNavLinkClick}>About Us</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/contact" onClick={handleNavLinkClick}>Contact</Link>
+              </li>
             </ul>
           </div>
         </div>
