7cf70187b30cc90c96ebb55bf9746bbfcd62d877
24Hoshiko <2005gaganjotkaur@gmail.com>
2024-09-17 17:58:11 +0530

    Padding added

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
   const [scrolled, setScrolled] = useState(false);
+  const location = useLocation();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -43,22 +44,46 @@ function Header() {
           >
             {/* Custom SVG Icon */}
             <svg data-wf-icon="Menu24Icon" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" clip-rule="evenodd" d="M19 7H5V6H19V7ZM19 12H5V11H19V12ZM19 17H5V16H19V17Z" fill="currentColor"></path>
+              <path fillRule="evenodd" clipRule="evenodd" d="M19 7H5V6H19V7ZM19 12H5V11H19V12ZM19 17H5V16H19V17Z" fill="currentColor"></path>
             </svg>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0 py-3 justify-content-end">
               <li className="nav-item">
-                <Link className="nav-link" to="/" onClick={handleNavLinkClick}>Home</Link>
+                <Link
+                  className={`nav-link ${location.pathname === '/' ? 'active' : ''}`}
+                  to="/"
+                  onClick={handleNavLinkClick}
+                >
+                  Home
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/events" onClick={handleNavLinkClick}>Events</Link>
+                <Link
+                  className={`nav-link ${location.pathname === '/events' ? 'active' : ''}`}
+                  to="/events"
+                  onClick={handleNavLinkClick}
+                >
+                  Events
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/about" onClick={handleNavLinkClick}>About Us</Link>
+                <Link
+                  className={`nav-link ${location.pathname === '/about' ? 'active' : ''}`}
+                  to="/about"
+                  onClick={handleNavLinkClick}
+                >
+                  About Us
+                </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/contact" onClick={handleNavLinkClick}>Contact</Link>
+                <Link
+                  className={`nav-link ${location.pathname === '/contact' ? 'active' : ''}`}
+                  to="/contact"
+                  onClick={handleNavLinkClick}
+                >
+                  Contact
+                </Link>
               </li>
             </ul>
           </div>
