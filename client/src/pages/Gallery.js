40fcfe9f4adda839ada85736ca226c895577bb22
24Hoshiko <2005gaganjotkaur@gmail.com>
2024-10-17 23:23:43 +0530

    IH 2024 images added.

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,32 +1,33 @@
-import { Carousel } from '../components/Carousel'
-import './Gallery.scss'
-import galleryData from  './galleryData.json'
+import { Carousel } from '../components/Carousel';
+import './Gallery.scss';
+import galleryData from './galleryData.json';
 
 export const Gallery = () => {
-    return(
+    return (
         <div className="gallery-container">
-             <div className="gallery-heading-container">
-                <span style={{fontSize: '50px', fontWeight: 'bold', letterSpacing: '0.2rem'}}>Gallery</span>
-                <p style={{fontSize: '20px', fontWeight: 'underline', letterSpacing: '0.1rem'}}>(soon will be updated with event images)</p>
-                <p style={{fontSize: '20px', fontWeight: 'underline', letterSpacing: '0.1rem'}}>These are all the amazing photos that were taken on our events</p>
-                <hr></hr>
+            <div className="gallery-heading-container">
+                <span style={{ fontSize: '50px', fontWeight: 'bold', letterSpacing: '0.2rem' }}>Gallery</span>
+                <p style={{ fontSize: '20px', fontWeight: 'underline', letterSpacing: '0.1rem' }}>
+                    These are all the amazing photos that were taken at our events
+                </p>
+                <hr />
             </div>
             <div className='gallery-blocks-container'>
-            {Object.entries(galleryData).map(([heading, data]) => {
-                const [title, description] = heading.split('-')
-                return(
-                    <div className="gallery-block" key={heading}>
-                        <div className='gallery-content'>
-                            <h2>{title}</h2>
-                            <p>{description}</p>
+                {Object.entries(galleryData).map(([heading, data]) => {
+                    const [title, description] = heading.split('-');
+                    return (
+                        <div className="gallery-block" key={heading}>
+                            <div className='gallery-content'>
+                                <h2>{title}</h2>
+                                <p>{description}</p>
+                            </div>
+                            <div className='carousel-container'>
+                                <Carousel className="carousel" data={data} />
+                            </div>
                         </div>
-                        <div className='carousel-container'>
-                            <Carousel className="carousel" data={data} />
-                        </div>
-                    </div>
-                )
-            })}
+                    );
+                })}
             </div>
         </div>
-    )
-}
+    );
+};
